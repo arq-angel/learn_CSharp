2b22cc350588608b37c22386fff10f9938c60758
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-22 22:22:43 +1100

    3. variables

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,20 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.Write("Hey!");
-Console.WriteLine("Hello World!");
+int x;  // declaration
+x = 123;    // initialization
 
-// This is a comment
+int y = 321;    // declaration + initialization
 
-/*
- * This
- * is
- * a
- * multiline
- * comment
- */
- 
- Console.WriteLine("\tBroCode");
- Console.WriteLine("Bro\bCode");
- Console.WriteLine("Bro\nCode");
- 
- Console.ReadKey();
+int z = x + y;
+
+Console.WriteLine(x);
+Console.WriteLine(y);
+Console.WriteLine(x + y);
+Console.WriteLine(z);
+
+int age = 21;   // whole integer
+
+Console.WriteLine("Your age is " + age);
+
+double height = 300.5;  // decimal number
+
+Console.WriteLine("Your height is " + height + "cm");
+
+bool alive = true;  // boolean value - true or false
+
+Console.WriteLine("Are you alive? " + alive);
+
+char symbol = '@';  // inside single quotes - only single character
+
+Console.WriteLine("Your Symbol is " + symbol);
+
+string name = "Arthur";
+
+Console.WriteLine("Hello " + name);
+
+string userName = symbol + name;
+
+Console.WriteLine("Your username is " + userName);
+
+Console.ReadKey();
