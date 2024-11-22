f660595b451b0dfccbf5caf07915377101118ef8
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-22 22:13:44 +1100

    2. output

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,5 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("I Like Pizza");
-Console.WriteLine("Its really good");
-Console.Beep();
+Console.Write("Hey!");
+Console.WriteLine("Hello World!");
+
+// This is a comment
+
+/*
+ * This
+ * is
+ * a
+ * multiline
+ * comment
+ */
+ 
+ Console.WriteLine("\tBroCode");
+ Console.WriteLine("Bro\bCode");
+ Console.WriteLine("Bro\nCode");
+ 
+ Console.ReadKey();
