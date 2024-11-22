f387355f3ffe82b59f301db98c642f9159b61242
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-22 22:24:40 +1100

    4. constants

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,39 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int x;  // declaration
-x = 123;    // initialization
+// constants = immutable values which are known at compile time
+//             and do not change for the life of the program
 
-int y = 321;    // declaration + initialization
+const double pi = 3.14159;
 
-int z = x + y;
-
-Console.WriteLine(x);
-Console.WriteLine(y);
-Console.WriteLine(x + y);
-Console.WriteLine(z);
-
-int age = 21;   // whole integer
-
-Console.WriteLine("Your age is " + age);
-
-double height = 300.5;  // decimal number
-
-Console.WriteLine("Your height is " + height + "cm");
-
-bool alive = true;  // boolean value - true or false
-
-Console.WriteLine("Are you alive? " + alive);
-
-char symbol = '@';  // inside single quotes - only single character
-
-Console.WriteLine("Your Symbol is " + symbol);
-
-string name = "Arthur";
-
-Console.WriteLine("Hello " + name);
-
-string userName = symbol + name;
-
-Console.WriteLine("Your username is " + userName);
+Console.WriteLine(pi);
 
 Console.ReadKey();
