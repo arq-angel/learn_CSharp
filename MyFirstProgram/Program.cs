0a1e0037732b4ced19f4236ef704da13bd20b720
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-25 17:40:54 +1100

    6. user input

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,49 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// type casting = Converting a value to a different data type
-//                Useful when we accept user input (string)
-//                Different data types can do different things
+Console.WriteLine("What's your name?");
+String name = Console.ReadLine();
 
-double a = 3.14;
+Console.WriteLine("What's your age?");
+int age = Convert.ToInt32(Console.ReadLine());
 
-int b = Convert.ToInt32(a);
-
-Console.WriteLine(b);
-
-Console.WriteLine(a.GetTypeCode());
-
-Console.WriteLine(b.GetTypeCode());
-
-int c = 123;
-
-double d = Convert.ToDouble(c) + 0.1;
-
-Console.WriteLine(d);
-
-Console.WriteLine(d.GetTypeCode());
-
-int e = 321;
-
-string f = Convert.ToString(e);
-
-Console.WriteLine(f);
-
-Console.WriteLine(f.GetTypeCode());
-
-string g = "$";
-
-char h = Convert.ToChar(g);
-
-Console.WriteLine(h);
-
-Console.WriteLine(h.GetTypeCode());
-
-string i = "true";
-
-bool j = Convert.ToBoolean(i);
-
-Console.WriteLine(j);
-
-Console.WriteLine(j.GetTypeCode());
+Console.WriteLine("Hello " + name + "!");
+Console.WriteLine("You are " + age + " years old.");
 
 Console.ReadKey();
