aeb25477456de6ed3173cbc028b9f19337db9035
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-25 19:02:43 +1100

    18. number guessing game

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -1,25 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// nested loops = loops inside of other loops
-//                Uses vary. Used a lot in sorting algorithms
+Random random = new Random();
+bool playAgain = true;
+int min = 1;
+int max = 100;
+int guess;
+int number;
+int guesses;
+String response;
 
-Console.WriteLine("How many rows?: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("How many columns?: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("What symbol?: ");
-String symbol = Console.ReadLine();
-
-for (int i = 0; i < rows; i++)
+while (playAgain)
 {
-    for (int j = 0; j < columns; j++)
+    guess = 0;
+    guesses = 0;
+    response = "";
+    number = random.Next(min, max);
+
+    while (guess != number)
     {
-        Console.Write(symbol);
+        Console.WriteLine("Guess a number between " + min + " - " + max + " : " );
+        guess = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Guess:" + guess);
+
+        if (guess > number)
+        {
+            Console.WriteLine(guess + " is too high!");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine(guess + " is too low!");
+        }
+        guesses++;
     }
-    Console.WriteLine();
+    Console.WriteLine("Number: " + number );
+    Console.WriteLine("YOU WIN!");
+    Console.WriteLine("Guesses: " + guesses);
+    
+    Console.WriteLine("Would you like to play again? (Y/N): ");
+    response = Console.ReadLine();
+    response = response.ToUpper();
+
+    if (response == "Y")
+    {
+        playAgain = true;
+    }
+    else
+    {
+        playAgain = false;
+    }
 }
 
+Console.WriteLine("Thanks for playing! ... I guess");
 
 Console.ReadKey();
