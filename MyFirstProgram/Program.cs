7e1ad11c7d9c8708fff36868dcae435b5d013bc4
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-26 16:25:12 +1100

    32. objects

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -6,14 +6,44 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            // class = A bundle of related code
-            //         Can be used as a blueprint to create objects (OOP)
-
-            Messages.Hello();
-            Messages.Waiting();
-            Messages.Bye();
+            // object = An instance of a class
+            //          A class can be used as a blueprint to create objects (OOP)
+            //          objects can have fields & methods (characteristics & actions)
+            
+            Human human1 = new Human();
+            Human human2 = new Human();
+            
+            human1.name = "Rick";
+            human1.age = 65;
+            
+            human2.name = "Morty";
+            human2.age = 16;
+            
+            human1.Eat();
+            human1.Sleep();
+            
+            human2.Eat();
+            human2.Sleep();
         
             Console.ReadKey();
         }
+    }
+
+    class Human
+    {
+        public String name;
+        public int age;
+
+        public void Eat()
+        {
+            Console.WriteLine(name + " is eating");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + " is sleeping");
+        }
+        
+        
     }
 }
