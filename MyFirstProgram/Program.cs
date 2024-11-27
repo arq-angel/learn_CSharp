cbe2de969745baf64f47c04630a929b653ff9b6a
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-11-27 17:55:26 +1100

    48. enums

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,25 +7,59 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-            // Auto-Implemented properties = shortcut when no additional logic is required in the property
-            //                               you do not have to define a field for a property
-            //                               you only have to write get; and/or set; inside the property
-            
-            Car car = new Car("Porsche");
+            // enums = special "class" that contains a set of name integer constants.
+            //         Use enums when you have values that you know will not change,
+            //         To get the integer value from an item, you must explicitly convert to an int
 
-            Console.WriteLine(car.Model);
+            //          name = integer
+
+            /*Console.WriteLine(Planets.Pluto + " is a planet");
+            Console.WriteLine(Planets.Mercury + " is planet #" + (int)Planets.Mercury);
+            Console.WriteLine(Planets.Pluto + " is planet #" + (int)Planets.Pluto);*/
+            
+            String name = PlanetRadius.Earth.ToString();
+            int radius = (int)PlanetRadius.Earth;
+            double volume = Volume(PlanetRadius.Earth);
+            
+            
+            Console.WriteLine("Planet: " + name);
+            Console.WriteLine("Radius: " + radius + "km");
+            Console.WriteLine("Volume: " + volume + "km^3");
 
             Console.ReadKey();
         }
+
+        public static double Volume(PlanetRadius radius)
+        {
+            double volume = (4.0/3.0) * Math.PI * Math.Pow((int)radius, 3);
+            
+            return volume;
+        }
     }
 
-    class Car
+    enum Planets
     {
-        public String Model { get; set; }
-        
-        public Car(String model)
-        {
-            this.Model = model;
-        }
+        Mercury = 1,
+        Venus = 2,
+        Earth = 3,
+        Mars = 4,
+        Jupiter = 5,
+        Saturn = 6,
+        Uranus = 7,
+        Neptune = 8,
+        Pluto = 9
+    }
+
+    enum PlanetRadius
+    {
+        Mercury = 2439,
+        Venus = 6051,
+        Earth = 6371,
+        Mars = 3389,
+        Jupiter = 69911,
+        Saturn = 58232,
+        Uranus = 25362,
+        Neptune = 24622,
+        Pluto = 1188
     }
 }
